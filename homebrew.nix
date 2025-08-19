a43e64e35553825b1fc4b925206fb84b49a96695
Mike Boorman <mike@mikebravo.dev>
2025-08-19 19:42:32 -0400

    Homebrew now working declaratively through nix.

--- homebrew.nix
+++ homebrew.nix
@@ -2,34 +2,39 @@
 
 { homebrew-core, homebrew-cask,... }:
 {
-  nix-homebrew = {
+  homebrew = {
     # Install Homebrew under the default prefix
     enable = true;
 
     # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-    enableRosetta = true;
+    # enableRosetta = true;
 
     # User owning the Homebrew prefix
     user = "mikebravo";
 
     # Automatically migrate existing Homebrew installations
-    autoMigrate = true;
+    # autoMigrate = true;
     # Optional: Declarative tap management
-    taps = {
-      "homebrew/homebrew-core" = homebrew-core;
-      "homebrew/homebrew-cask" = homebrew-cask;
-    };
+    taps = [
+      "homebrew/homebrew-core"
+      "homebrew/homebrew-cask"
+    ];
 
     # Optional: Enable fully-declarative tap management
     #
     # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
-    mutableTaps = false;
+    # mutableTaps = false;
     # You can also manage taps, casks, and formulas here
-    # taps = [
-    #   "homebrew/services"
-    # ];
-    # casks = [
-    #   "google-chrome"
-    # ];
+    #taps = [
+    #  "homebrew/services"
+    #];
+    casks = [
+      "ungoogled-chromium"
+      "google-chrome"
+      "google-chrome@dev"
+      "signal"
+      "signal@beta"
+      "session"
+    ];
   };
 }
